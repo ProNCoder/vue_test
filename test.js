b3dbb04da22bab58f04024f70d3e21e351816bfe
ProNCoder <normatjonqozimurodovgit@gmail.com>
2022-06-05 23:38:26 +0500

    5iyun

--- test.js
+++ test.js
@@ -1,8 +1,32 @@
 const app = Vue.createApp({
     data() {
         return {
-            name: 'salom n'
+            nameBool: true,
+            name: 'salom hammaga'
         }
     },
+    methods: {
+        tekshir() {
+            this.nameBool = !this.nameBool
+        }
+    },
+
 })
+
 app.mount('#app')
+
+//4 dars bu inkrement amali va tashqaridan uzgaruvchi berish
+// data() {
+//     return {
+//         age: 22,
+//         name: 'salom n'
+//     }
+// },
+// methods: {
+//     oshir() {
+//         this.age = this.age + 1
+//     },
+//     qushish(name) {
+//         this.name = name
+//     }
+// },
